196ebfe274e8eb33aae7cdd2807754880bacf9da
t3rmian <t3rmian@gmail.com>
2019-05-20 19:43:15 +0200

    Reconfigure

--- src/template.config.js
+++ src/template.config.js
@@ -1,15 +1,15 @@
 export default {
-  siteTitle: "react-static-teapot",
-  siteLongTitle: "react-static-teapot: blog template",
-  siteRoot: "https://react-static-teapot.netlify.com",
+  siteTitle: "418",
+  siteLongTitle: "418: devblog",
+  siteRoot: "https://418.netlify.com",
   defaultLanguage: "en",
 
   optional: {
-    commentsRepo: "t3rmian/react-static-teapot",
-    ga: "UA-73928706-6",
+    commentsRepo: "t3rmian/blog",
+    // ga: "UA-73928706-6",
     twitterAuthor: "t3rmian",
-    braveRewardsToken:
-      "8b5efef3f30421b88d644c7a469434a562cb4935f83c122327ce4b34aba0cb0c",
+    // braveRewardsToken:
+    //   "8b5efef3f30421b88d644c7a469434a562cb4935f83c122327ce4b34aba0cb0c",
     disallow: "Disallow: /",
   }
 };
