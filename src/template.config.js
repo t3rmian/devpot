12b356b507d0467332ef2cb28017ce178b9ce68e
t3rmian <terlecki@termian.dev>
2020-03-14 20:27:04 +0100

    Take advantage of branch previews

--- src/template.config.js
+++ src/template.config.js
@@ -1,6 +1,6 @@
 export default {
   author: "Damian Terlecki",
-  authorSite: "https://termian.dev",
+  authorSite: isPreview() ? process.env.DEPLOY_PRIME_URL : "https://termian.dev",
   siteTitle: "Devpot",
   siteLongTitle: "Devpot: a coder's blog",
   siteRoot: "https://blog.termian.dev",
@@ -8,9 +8,13 @@ export default {
 
   optional: {
     commentsRepo: "t3rmian/devpot",
-    ga: "UA-73928706-7",
+    ga: isPreview() ? null : "UA-73928706-7",
     twitterAuthor: "t3rmian",
     braveRewardsToken:
       "d8168da8e5089e3919156575ad5514238efa9fcd453950683ab55ebbf2be54e5",
   }
 };
+
+function isPreview() {
+  return (process && process.env && process.env.DEPLOY_PRIME_URL);
+}
