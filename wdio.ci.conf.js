67ce376e75ec450ef53671fab4cd95b40793b863
t3rmian <terlecki@termian.dev>
2020-03-29 20:06:50 +0200

    Switch to chrome for smoke tests

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -55,7 +55,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'firefox',
+        browserName: 'chrome',
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
