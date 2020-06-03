9b4371317a2b5dcad8fc7228b885a27ebfe6d490
t3rmian <terlecki@termian.dev>
2020-06-03 23:04:22 +0200

    Adjust tests to dynamic nature of tested elements

--- test/steps/comments.js
+++ test/steps/comments.js
@@ -10,9 +10,23 @@ When('I wait for comments load', () => {
   waitForLoad('article:first-of-type');
 });
 Then('I should see the comment authors and the contents', () => {
+  browser.waitUntil(
+    () => {
+      return $('main').getText().include("t3rmian commented")
+    },
+    5000,
+    "Expected loading finish"
+  );
   expect($('main').getText()).to.include("t3rmian commented")
   expect($('main').getText()).to.include("Start the TNS Listener service")
 });
 Then('I should see the comment submit option', () => {
+  browser.waitUntil(
+    () => {
+      $('main').getText().include("Sign in to comment")
+    },
+    5000,
+    "Expected loading finish"
+  );
   expect($('main').getText()).to.include("Sign in to comment");
 });
