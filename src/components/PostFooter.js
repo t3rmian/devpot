f3f785ca306361a97aa52292453ebba326a6ffea
t3rmian <terlecki@termian.dev>
2020-06-06 13:58:05 +0200

    Add more styling around button icons, move author to the top, add author picture, refactor post header and footer

---
+++ src/components/PostFooter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "components/Router";
+
+export default ({ prev, next, source }) => (
+  <div className="post-footer">
+    <span>{prev && <Link to={prev.url}>&lt; {prev.title}</Link>}</span>
+    <span>{source && <Link to={source.url}>{source.title}</Link>}</span>
+    <span>{next && <Link to={next.url}>{next.title} &gt;</Link>}</span>
+  </div>
+);
---
+++ src/components/PostHeader.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "components/Router";
+
+export default ({
+  tags,
+  routeTags,
+  title,
+  date,
+  dateFormatted,
+  updated,
+  updatedFormatted,
+  minutesRead,
+  authorSite,
+  authorPicture,
+  author,
+  authorName = author.split(" ")[0],
+  authorSurname = author.split(" ")[1],
+}) => (
+  <div className="header">
+    <h2 className="title">{title}</h2>
+    {tags && (
+      <div className="tags">
+        {tags.map((tag) => (
+          <Link
+            className="item"
+            key={tag}
+            to={`${routeTags.find((t) => t.value === tag).path}`}
+          >
+            {tag}
+          </Link>
+        ))}
+      </div>
+    )}
+    <div className="meta">
+      <span className="item">
+        {updated && (
+          <time dateTime={new Date(updated).toISOString()}>
+            {updatedFormatted}
+            <br />
+          </time>
+        )}
+        <time dateTime={new Date(date).toISOString()}>{dateFormatted}</time>
+      </span>
+      <span className="item profile">
+        <a href={authorSite} className="profile">
+          <span>{authorName}</span>
+          {authorPicture && <img crossOrigin="true" src={authorPicture} />}
+          <span>{authorSurname}</span>
+        </a>
+      </span>
+
+      <span className="item">{minutesRead}</span>
+    </div>
+  </div>
+);
