1724e1cbe315bd069ded1cabb842e84b271c0e7f
t3rmian <terlecki@termian.dev>
2020-06-27 20:39:57 +0200

    Fix PWA scope

--- public/pwabuilder-sw-register.js
+++ public/pwabuilder-sw-register.js
@@ -2,7 +2,7 @@ if ("serviceWorker" in navigator) {
   if (!navigator.serviceWorker.controller) {
     navigator.serviceWorker
       .register("/pwabuilder-sw.js", {
-        scope: "./"
+        scope: "/"
       });
   }
 }
