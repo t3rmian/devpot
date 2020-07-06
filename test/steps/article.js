1b854c10ae0e075593f1a00fe31e968ca930d3ef
t3rmian <terlecki@termian.dev>
2020-07-06 21:10:57 +0200

    Add another test for next link verification

--- test/steps/article.js
+++ test/steps/article.js
@@ -1,8 +1,17 @@
 const { Given, When, Then } = require('cucumber');
 
 When('I open first article', () => {
-  const link = $$('main a')[0];
-  this.secondArticleTitle = $$('main a')[1].getText();
+  const links = $$('main a');
+  const link = links[0];
+  this.secondArticleTitle = links[1].getText();
+  link.getText();
+  link.click();
+});
+
+When('I open last article', () => {
+  const links = $$('main a');
+  const link = links[links.length - 1];
+  this.secondArticleTitle = links[links.length - 2].getText();
   link.getText();
   link.click();
 });
@@ -18,5 +27,21 @@ When('I click a link to a previous article', () => {
 });
 
 Then('I should see the previous article', () => {
+  expect($$('.title')[0].getText().toUpperCase()).to.include(this.secondArticleTitle.toUpperCase());
+});
+
+Then('I should see a link to a next article', () => {
+  const footerLinks = $$('.post-footer a');
+  const link = footerLinks[footerLinks.length - 1];
+  expect(link.getText().toUpperCase()).to.include(this.secondArticleTitle.toUpperCase());
+});
+
+When('I click a link to a next article', () => {
+  const footerLinks = $$('.post-footer a');
+  const link = footerLinks[footerLinks.length - 1];
+  link.click();
+});
+
+Then('I should see the next article', () => {
   expect($$('.title')[0].getText().toUpperCase()).to.include(this.secondArticleTitle.toUpperCase());
 });
