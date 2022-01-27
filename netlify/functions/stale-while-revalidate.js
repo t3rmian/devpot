17254b3ba601847722f86084e03dad178a2af60e
Damian Terlecki <terlecki@termian.dev>
2022-01-27 19:42:25 +0100

    Add vary header to the SWR test

--- netlify/functions/stale-while-revalidate.js
+++ netlify/functions/stale-while-revalidate.js
@@ -5,6 +5,7 @@ exports.handler = async function (event) {
         headers: {
             "Cache-Control": "max-age=1, stale-while-revalidate=60",
             "Access-Control-Allow-Origin": "*",
+            "Vary": "Accept-Language",
             "X-Robots-Tag": "noindex",
         },
         body: lang.includes("de") ? "Hallo Welt!" : lang.includes("pl") ? "Witaj Świecie!" : "Hello World!",
