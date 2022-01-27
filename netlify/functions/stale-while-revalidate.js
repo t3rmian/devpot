1ef28335de9ef1279b39d7b94017f1be52dcf02a
Damian Terlecki <terlecki@termian.dev>
2022-01-27 20:05:44 +0100

    Add conditional vary to the SWR test

--- netlify/functions/stale-while-revalidate.js
+++ netlify/functions/stale-while-revalidate.js
@@ -1,13 +1,17 @@
 exports.handler = async function (event) {
     const lang = event.headers['accept-language'] || 'en'
-    return {
+    const vary = event['queryStringParameters'].vary
+    const response = {
         statusCode: 200,
         headers: {
             "Cache-Control": "max-age=1, stale-while-revalidate=60",
             "Access-Control-Allow-Origin": "*",
-            "Vary": "Accept-Language",
             "X-Robots-Tag": "noindex",
         },
-        body: lang.includes("de") ? "Hallo Welt!" : lang.includes("pl") ? "Witaj Świecie!" : "Hello World!",
+        body: (lang.includes("de") ? "Hallo Welt!" : lang.includes("pl") ? "Witaj Świecie!" : "Hello World!" ) + " " + new Date().getTime(),
     };
+    if (vary) {
+        response.headers["Vary"] = "Accept-Language";
+    }
+    return response;
 }
